9a8a9a966a1c6be405fe7a9dd6fc751273cbdccd
CruzanCaramele <letters2hamza@gmail.com>
2015-04-03 18:42:44 +0800

    Create class and add variable for landing page

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 # Create your views here.
+
+class LandingPage(TemplateView):
+	template_name = "base/index.html"
